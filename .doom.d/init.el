e083f9aadee95c9697c272d7db99abf0c368919f
Andre Peric Tavares <andre.peric.tavares@gmail.com>
2020-06-16 21:43:40 -0300

    Add hydra to Doom init file

--- .doom.d/init.el
+++ .doom.d/init.el
@@ -31,7 +31,7 @@
        doom-quit         ; DOOM quit-message prompts when you quit Emacs
        ;;fill-column       ; a `fill-column' indicator
        hl-todo           ; highlight TODO/FIXME/NOTE/DEPRECATED/HACK/REVIEW
-       ;;hydra
+       hydra
        indent-guides     ; highlighted indent columns
        ;;minimap           ; show a map of the code on the side
        modeline          ; snazzy, Atom-inspired modeline, plus API
