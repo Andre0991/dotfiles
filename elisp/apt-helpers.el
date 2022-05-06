3fbffcb9965f7db588374b913b33c9dddfd8b5c2
Andre Peric Tavares <andre.peric.tavares@gmail.com>
2022-05-06 16:31:24 -0300

    [Emacs] Add helper function for deleting file and buffer

--- elisp/apt-helpers.el
+++ elisp/apt-helpers.el
@@ -21,4 +21,21 @@
   "Comment out one or more s-expressions."
   nil)
 
+;; from https://emacsredux.com/blog/2013/04/03/delete-file-and-buffer/
+(defun apt-delete-file-and-buffer ()
+  "Kill the current buffer and deletes the file it is visiting."
+  (interactive)
+  (let ((filename (buffer-file-name)))
+    (when filename
+      (if (vc-backend filename)
+          (vc-delete-file filename)
+        (progn
+          (delete-file filename)
+          (message "Deleted file %s" filename)
+          (kill-buffer))))))
+
+(global-set-key (kbd "C-c f D")  #'apt-delete-file-and-buffer)
+
+
+
 (provide 'apt-helpers)
