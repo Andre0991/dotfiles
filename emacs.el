c7c6592d7391db7ad6632b5347339129b55144e3
Andre Peric Tavares <andre.peric.tavares@gmail.com>
2024-10-14 20:14:39 -0300

    wip

--- emacs.el
+++ emacs.el
@@ -25,6 +25,7 @@
                                   consult
                                   corfu
                                   cue-mode
+                                  dape
                                   denote
                                   diminish
                                   eat
@@ -623,6 +624,50 @@ for better naming in the hooks it is listed."
             ;; See https://github.com/joaotavora/eglot/discussions/894
             #'apt-eglot-hooks)
   )
+
+(use-package dape
+  ;; :preface
+  ;; By default dape shares the same keybinding prefix as `gud'
+  ;; If you do not want to use any prefix, set it to nil.
+  ;; (setq dape-key-prefix "\C-x\C-a")
+
+  ;; :hook
+  ;; Save breakpoints on quit
+  ;; ((kill-emacs . dape-breakpoint-save)
+  ;; Load breakpoints on startup
+  ;;  (after-init . dape-breakpoint-load))
+
+  ;; :config
+  ;; Turn on global bindings for setting breakpoints with mouse
+  ;; (dape-breakpoint-global-mode)
+
+  ;; Info buffers to the right
+  ;; (setq dape-buffer-window-arrangement 'right)
+
+  ;; Info buffers like gud (gdb-mi)
+  ;; (setq dape-buffer-window-arrangement 'gud)
+  ;; (setq dape-info-hide-mode-line nil)
+
+  ;; Pulse source line (performance hit)
+  ;; (add-hook 'dape-display-source-hook 'pulse-momentary-highlight-one-line)
+
+  ;; Showing inlay hints
+  ;; (setq dape-inlay-hints t)
+
+  ;; Save buffers on startup, useful for interpreted languages
+  ;; (add-hook 'dape-start-hook (lambda () (save-some-buffers t t)))
+
+  ;; Kill compile buffer on build success
+  ;; (add-hook 'dape-compile-hook 'kill-buffer)
+
+  ;; Projectile users
+  ;; (setq dape-cwd-fn 'projectile-project-root)
+  )
+
+;; Enable repeat mode for more ergonomic `dape' use
+;; (use-package repeat
+;;   :config
+;;   (repeat-mode))
 
 (use-package jarchive
   :config
