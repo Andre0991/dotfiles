21cb0d65c03091541ee8c232a0cc2f5e6abe45de
Andre Peric Tavares <andre.peric.tavares@gmail.com>
2025-07-22 14:10:28 -0300

    wip

--- makesymlinks.sh
+++ makesymlinks.sh
@@ -1,16 +1,19 @@
 #!/bin/bash
 
+# Default prefix, can be overridden by first argument
+PREFIX=${1:-~/dev/peric}
+
 # set -o pipefail
 # mkdir -p ~/.config/karabiner
-# ln -s ~/dev/peric/dotfiles/karabiner.json ~/.config/karabiner/karabiner.json
+# ln -s ${PREFIX}/dotfiles/karabiner.json ~/.config/karabiner/karabiner.json
 
 # todo: make this script idempotent, consider prefix dir from home/work
 
-ln -s ~/dotfiles/.vimrc ~/.vimrc
-ln -s ~/dev/dotfiles/emacs.el ~/.emacs.el
+ln -s ${PREFIX}/dotfiles/.vimrc ~/.vimrc
+ln -s ${PREFIX}/dotfiles/emacs.el ~/.emacs.el
 
 mkdir -p ~/.hammerspoon/
-ln -sf ~/dev/dotfiles/hammerspoon/init.lua ~/.hammerspoon/init.lua
-ln -s ~/dev/dotfiles/cursor/mcp.json ~/.cursor/mcp.json
+ln -sf ${PREFIX}/dotfiles/hammerspoon/init.lua ~/.hammerspoon/init.lua
+ln -s ${PREFIX}/dotfiles/cursor/mcp.json ~/.cursor/mcp.json
 
 
